54b0bb9760fe7c495f8fef00f7925629725722f5
Artur <arturtres3@gmail.com>
2020-06-01 20:03:13 -0300

    Tira as boas vindas do main
    A página de boas vindas aparece ao abrir pela primeira vez e quando clicamos o botão na pagina principal

--- Quicktest/app/src/main/java/com/example/quicktest/MainActivity.java
+++ Quicktest/app/src/main/java/com/example/quicktest/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.quicktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String PREFS_NAME = "MyPrefsFile";
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (settings.getBoolean("my_first_time", true)) {
+            setContentView(R.layout.activity_tela_bem_vindo);
+
+            settings.edit().putBoolean("my_first_time", false).commit();
+        }else{
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    public void boasVindas(android.view.View view){
+        Intent bemVindo = new Intent(this, telaBemVindo.class);
+
+        startActivity(bemVindo);
     }
 }
