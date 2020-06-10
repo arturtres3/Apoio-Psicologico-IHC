8220efa3e2d8964304ac41a350ccd36271c11232
Thiago Oliveira <thiagoal.ol@hotmail.com>
2020-06-10 20:58:15 -0300

    Na página inicial, falta só reposicionar elementos

--- Quicktest/app/src/main/java/com/example/quicktest/telaBemVindo.java
+++ Quicktest/app/src/main/java/com/example/quicktest/telaBemVindo.java
@@ -2,7 +2,10 @@ package com.example.quicktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.quicktest.ui.login.LoginActivity;
 
 public class telaBemVindo extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class telaBemVindo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_bem_vindo);
+    }
+
+    public void fazerLogin(android.view.View view){
+        Intent userLogin = new Intent(telaBemVindo.this, LoginActivity.class);
+
+        startActivity(userLogin);
     }
 }
