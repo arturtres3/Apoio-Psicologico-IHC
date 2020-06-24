887961749310d33815849784526189b6dd592298
Thiago Oliveira <thiagoal.ol@hotmail.com>
2020-06-24 14:01:29 -0300

    Layout da página de edição de perfil parcialmente feito

--- Quicktest/app/src/main/java/com/example/quicktest/EditProfile.java
+++ Quicktest/app/src/main/java/com/example/quicktest/EditProfile.java
@@ -1,14 +1,24 @@
 package com.example.quicktest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditProfile extends AppCompatActivity {
 
+    private EditText doYouWork;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
     }
 }
+
+
+
+
+
+
