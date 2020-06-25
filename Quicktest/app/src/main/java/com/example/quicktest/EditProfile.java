5c0dd471ad0dd315b9a762fe3e590789e19104f4
Thiago Oliveira <thiagoal.ol@hotmail.com>
2020-06-25 15:43:36 -0300

    Edição de perfil feita com as informações que decidimos até agora

--- Quicktest/app/src/main/java/com/example/quicktest/EditProfile.java
+++ Quicktest/app/src/main/java/com/example/quicktest/EditProfile.java
@@ -8,10 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EditProfile extends AppCompatActivity {
 
-    private EditText doYouWork;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +35,28 @@ public class EditProfile extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
+
+    public void howManyHoursWork(View view){
+        final float HOWMANYHOURS_TEXTSIZE = 20;
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
+        TextView howManyHoursText = new TextView(EditProfile.this);
+        howManyHoursText.setText("Tudo bem, mas quantas horas por dia de trabalho?");
+        howManyHoursText.setTextSize(HOWMANYHOURS_TEXTSIZE);
+
+        final EditText howManyHoursAnswer = new EditText(EditProfile.this);
+        howManyHoursAnswer.setHint("Insira apenas um número de horas(ex: 8)");
+        howManyHoursAnswer.setTextSize(14);
+
+        linearLayout.addView(howManyHoursText);
+        linearLayout.addView(howManyHoursAnswer);
+    }
+
+    public void doNotWork(View view){
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
+        linearLayout.removeAllViews();
+    }
+
+
 }
 
 
